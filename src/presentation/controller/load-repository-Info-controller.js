9f92ba729fb1c01b9e04c505783ea0196792c8ed
Flavio Duarte <flduarte13@gmail.com>
2021-05-18 23:30:41 -0300

    feat: ensure LoadRepositoryInfoController calls LoadRepositoryInfo with correct values

--- src/presentation/controller/load-repository-Info-controller.js
+++ src/presentation/controller/load-repository-Info-controller.js
@@ -2,11 +2,17 @@ const { badRequest } = require('../helpers/http-helpers');
 const MissingParamError = require('../errors/missing-param-error');
 
 module.exports = class LoadRepositoryInfoController {
+  constructor({ loadRepositoryInfo }) {
+    this.loadRepositoryInfo = loadRepositoryInfo;
+  }
+
   async handle(httpRequest) {
     const { author, repository } = httpRequest.body;
 
     if (!author) return badRequest(new MissingParamError('author'));
     if (!repository) return badRequest(new MissingParamError('repository'));
+
+    await this.loadRepositoryInfo.load(author, repository);
 
     return null;
   }
